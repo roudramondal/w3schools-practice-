474a7fc1aed59c24efd66c4134ba987506394880
Roudra <103403881+roudramondal@users.noreply.github.com>
2024-10-27 19:32:40 +0600

    Add files via upload

---
+++ w3school2.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  char a=65,b=66,c=90;
+  cout<<a<<endl<<b<<endl<<c<<endl;
+  return 0;
+
+}
---
+++ w3school3.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  int item=50;
+  double c_per=9.99;
+  double t_c=item*c_per;
+  char currency = '$';
+  cout<<"Number of item= "<<item<<endl;
+  cout<<"cost per hour= "<<c_per<<endl;
+  cout<< "total cost= "<<currency<<t_c<<endl;
+
+}
